2df30c887295ec9baf8d9f274be8f893be094fef
Jarry Palacios <jarry.palacios@hotmail.com>
2019-05-16 18:28:07 -0400

    first commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+	const LBS ='Library/';
+	const VIEWS = './Views/';
+	define('DFT','default/');
+	define('URL','http://127.0.0.1/sisccombmu/');
+	define("SITE_NAME","SISCCOMBMU");
+	define("DOMAIN_NAME","http://127.0.0.1/sisccombmu/");
+
+	//bases de datos
+	define("KEY_DB","p4l4c10ssyst3ms");
+	define('DB_SERVER', 'localhost'); // eg, localhost - should not be empty for productive servers
+  	define('DB_SERVER_USERNAME', 'postgres');
+  	define('DB_SERVER_PASSWORD', '123456');
+  	define('DB_DATABASE', 'sisccombmu');
+  	define('DB_PORT', '5432');
+
+?>
